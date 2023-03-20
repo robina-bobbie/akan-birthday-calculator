98cd9d83f6ae944fd8a382bbd2094ca0f998aa13
Bobbie <robina.bosibori@student.moringaschool.com>
2023-03-20 18:25:48 +0300

    Added variables

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function submitForm() {
+    // variables
+    let gender = "";
+    let akanName = ";"
+    let proceed = true;
+
+    // get radio button state
+    var genderMale = document.getElementById("genderMale").checked;
+    var genderFemale = document.getElementById("genderFemale").checked;
+
+}
